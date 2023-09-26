b788b2999054d4518adac083f78e2d1131ea7aa7
Ivy <ivystacyn@gmail.com>
2023-09-26 13:43:56 +0000

    Task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,9 @@
 export default function createIteratorObject(report) {
-  // Initialize department and employee indices
-  let departmentIndex = 0;
-  let employeeIndex = 0;
-
-  // Generator function to iterate through employees
-  function* iterateEmployees() {
-    while (departmentIndex < Object.keys(report.allEmployees).length) {
-      const departmentName = Object.keys(report.allEmployees)[departmentIndex];
-      const employees = report.allEmployees[departmentName];
-
-      yield employees[employeeIndex];
-
-      // Move to the next employee in the current department
-      employeeIndex++;
-
-      // If we've gone through all employees in the current department, move to the next department
-      if (employeeIndex >= employees.length) {
-        employeeIndex = 0;
-        departmentIndex++;
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
-  }
-
-  return {
-    [Symbol.iterator]: iterateEmployees,
-  };
+  }());
 }
