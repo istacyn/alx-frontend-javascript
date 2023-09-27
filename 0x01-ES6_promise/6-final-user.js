6a26657c5af1032c23e8fc5cf6ebf2b21b7d4e74
Ivy <ivystacyn@gmail.com>
2023-09-27 17:38:53 +0000

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const resultArray = []
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => (
+      resultArray.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    ));
+    return resultArray;
+  });
+}
