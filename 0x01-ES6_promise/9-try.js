1ae608667f134e1364ad1401e300d30219c6c69b
Ivy <ivystacyn@gmail.com>
2023-09-27 18:18:44 +0000

    Task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction(), 'Guardrail was processed');
   } catch (error) {
-    queue.push(`Error: ${error.message}`, 'Guardrail was processed')
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
   return queue;
 }
