838906899d095f8180b4651643a9a6ec8b5e75c8
Ivy <ivystacyn@gmail.com>
2023-09-27 17:43:06 +0000

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const resultArray = []
+  const resultArray = [];
   return Promise.allSettled(promises).then((results) => {
     results.map(({ status, value, reason }) => (
       resultArray.push({
