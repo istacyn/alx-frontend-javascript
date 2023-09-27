9283451b3c08337d58f7f91b411e46c802d61219
Ivy <ivystacyn@gmail.com>
2023-09-27 13:20:28 +0000

    Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const responseData = { data: 'Sample response from API' };
-      resolve(responseData);
-    }, 1000);
-  });
+export default function getResponseFromAPI () {
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
-
-export default getResponseFromAPI;
