0c53c3d3aa0ad7cc3bf13c55a5a9a5a629322086
Ivy <ivystacyn@gmail.com>
2023-09-27 16:35:37 +0000

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+function handleResponseFromAPI(promise) {
+  const successPromise = { status: 200, body: 'success' };
+  return promise
+    .then(() => successPromise)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
+
+export default handleResponseFromAPI
